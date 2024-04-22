9bca4384ed4b66fb30ae9bae14af506c80ccb432
Ioannis Karakostas <85876945+giakara@users.noreply.github.com>
2024-04-22 12:12:15 +0300

    add-some-unit-tests

---
+++ LotteryProject.UnitTests/Fixtures/DBFixture.cs
@@ -0,0 +1,77 @@
+﻿using LotteryProject.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using static LotteryProject.EFCore.EntityDbContext;
+
+namespace LotteryProject.UnitTests.Fixtures
+{
+    public class DBFixture
+    {
+        private const string ConnectionString = "Data Source=localhost;Initial Catalog=TestLotteryProject;User ID=sa; Password=Lottery!123;TrustServerCertificate=true;";
+
+        private static readonly object _lock = new();
+        private static bool _databaseInitialized;
+
+        public DBFixture()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var context = CreateContext())
+                    {
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+                        context.AddRange(
+                            new Guest("test1", "test1")
+                            {
+                                Id = new Guid("3a39a232-b5f1-4f04-b07b-43c843185889")
+                            },
+                            new Guest("test2", "test2")
+                            {
+                                Id = new Guid("dd3f3be4-1094-459e-b3a6-a978aaf550a3")
+                            },
+                            new Present("testPresent1")
+                            {
+                                Id = new Guid("1ba7e5d5-69e1-4888-8e83-3e060d11e83c")
+                            },
+                            new Present("testPresent2")
+                            {
+                                Id = new Guid("50530ff8-9798-4dda-8a22-41ab36b5a0fa")
+                            },
+                            new Present("testPresent2")
+                            {
+                                Id = new Guid("fba7c0dc-4aba-45dd-aab2-dec90db19be8")
+                            },
+                        new Lottery
+                        {
+                            GuestID = new Guid("3a39a232-b5f1-4f04-b07b-43c843185889"),
+                            PresentID = new Guid("1ba7e5d5-69e1-4888-8e83-3e060d11e83c"),
+                            LotteryDate = DateTime.Now,
+                        },
+                        new Lottery
+                        {
+                            GuestID = new Guid("dd3f3be4-1094-459e-b3a6-a978aaf550a3"),
+                            PresentID = new Guid("50530ff8-9798-4dda-8a22-41ab36b5a0fa"),
+                            LotteryDate = DateTime.Now,
+                        });
+                        context.SaveChanges();
+                    }
+
+                    _databaseInitialized = true;
+                }
+            }
+        }
+
+        public DataContext CreateContext()
+            => new DataContext(
+                new DbContextOptionsBuilder<DataContext>()
+                    .UseSqlServer(ConnectionString)
+                    .Options);
+    }
+}
--- LotteryProject.UnitTests/Systems/Controllers/TestLotteriesController.cs
+++ LotteryProject.UnitTests/Systems/Controllers/TestLotteriesController.cs
@@ -1,7 +1,10 @@
-﻿using LotteryProject.EFCore.Services;
+﻿using FluentAssertions;
+using LotteryProject.EFCore.Services;
 using LotteryProject.Models.DTOs;
 using LotteryProject.Models.Entities;
+using LotteryProject.Models.Exceptions;
 using LotteryProject.Server.Controllers;
+using LotteryProject.UnitTests.Fixtures;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
@@ -9,29 +12,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
 
 namespace LotteryProject.UnitTests.Systems.Controllers
 {
-    public class TestLotteriesController
+    public class TestLotteriesController : IClassFixture<DBFixture>
     {
         private readonly Mock<ILotteryService> _mocklotteryService;
-
-        public TestLotteriesController()
+        private DBFixture _fixture;
+        public TestLotteriesController(DBFixture fixture)
         {
             _mocklotteryService = new Mock<ILotteryService>();
+            _fixture = fixture;
+
         }
-        //[Fact]
-        //public async Task Test_CreateLottery_Failed_ThrowLotteryPresentIsGiftedException()
-        //{
-        //    var guid = Guid.NewGuid();
-        //    var cancellationToken = new CancellationToken();
-        //    var lotterytoAdd = new AddEditLotteryDTO
-        //    {
-        //        PresentID = guid,
-        //    };
-        //    //Arrange
-        //    _mocklotteryService.Setup(service => service.CreateLottery(lotterytoAdd, cancellationToken))
-        //        .ReturnsAsync(new Lottery());
-        //}
+        [Fact]
+        public async void Test_CreateLottery_Failed_ThrowLotteryPresentIsGiftedException()
+        {
+            using var context = _fixture.CreateContext();
+            var guid = new Guid("1ba7e5d5-69e1-4888-8e83-3e060d11e83c");
+            var cancellationToken = new CancellationToken();
+            var lotterytoAdd = new AddEditLotteryDTO
+            {
+                PresentID = guid,
+            };
+
+            var lotteryService = new LotteryService(context);
+            await Assert.ThrowsAsync<LotteryPresentIsGiftedException>(() => lotteryService.CreateLottery(lotterytoAdd, cancellationToken));
+
+        }
+        [Fact]
+        public async void Test_CreateLottery_Failed_ThrowLotteryPresentIsRequiredException()
+        {
+            using var context = _fixture.CreateContext();
+            var guid = Guid.Empty;
+            var cancellationToken = new CancellationToken();
+            var lotterytoAdd = new AddEditLotteryDTO
+            {
+                PresentID = guid,
+            };
+
+            var lotteryService = new LotteryService(context);
+            await Assert.ThrowsAsync<LotteryPresentIsRequiredException>(() => lotteryService.CreateLottery(lotterytoAdd, cancellationToken));
+
+        }
+        [Fact]
+        public async void Test_CreateLottery_Failed_ThrowLotteryNoAvailableGuestsException()
+        {
+            using var context = _fixture.CreateContext();
+            var guid = new Guid("fba7c0dc-4aba-45dd-aab2-dec90db19be8");
+            var cancellationToken = new CancellationToken();
+            var lotterytoAdd = new AddEditLotteryDTO
+            {
+                PresentID = guid,
+            };
+
+            var lotteryService = new LotteryService(context);
+            await Assert.ThrowsAsync<LotteryNoAvailableGuestsException>(() => lotteryService.CreateLottery(lotterytoAdd, cancellationToken));
+
+        }
+        [Fact]
+        public async void Test_CreateLottery_Success()
+        {
+            using var context = _fixture.CreateContext();
+            var guestGuid = new Guid("c1ec69af-1e22-4a3f-a959-5772a8a1dc2e");
+            context.AddRange(new Guest("test3", "test3")
+            {
+                Id = guestGuid,
+            });
+            context.SaveChanges();
+            var guid = new Guid("fba7c0dc-4aba-45dd-aab2-dec90db19be8");
+            var cancellationToken = new CancellationToken();
+            var lotterytoAdd = new AddEditLotteryDTO
+            {
+                PresentID = guid,
+            };
+
+            var lotteryService = new LotteryService(context);
+            var successReturn = lotteryService.CreateLottery(lotterytoAdd, cancellationToken);
+            Assert.Equal(successReturn.Result.PresentID, lotterytoAdd.PresentID);
+
+        }
     }
 }
